48cffa421abf65649ef7ffc6e3445dcb893d1809
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2024-12-30 09:08:19 -0300

    encrypting password

--- src/main/java/com/fincontrol/demo/controllers/UsuarioController.java
+++ src/main/java/com/fincontrol/demo/controllers/UsuarioController.java
@@ -2,22 +2,31 @@ package com.fincontrol.demo.controllers;
 
 import com.fincontrol.demo.controllers.dtos.CreateUserDto;
 import com.fincontrol.demo.controllers.dtos.UpdateUserDto;
+import com.fincontrol.demo.models.Role;
 import com.fincontrol.demo.models.Usuario;
+import com.fincontrol.demo.repositories.RoleRepository;
 import com.fincontrol.demo.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class UsuarioController {
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/usuario")
     public List<Usuario> obterTodos(){return this.usuarioService.obterTodos();}
@@ -31,11 +40,14 @@ public class UsuarioController {
     @PostMapping(value = "/usuario")
     public ResponseEntity<Usuario> criarPaciente(@RequestBody CreateUserDto createUserDto){
 
+            var roleBasic = roleRepository.findByName(Role.Values.BASIC.name());
+
         var usuario = new Usuario();
             usuario.setCpf(createUserDto.cpf());
             usuario.setNome(createUserDto.nome());
             usuario.setEmail(createUserDto.email());
-            usuario.setPassword(createUserDto.password());
+            usuario.setPassword(passwordEncoder.encode(createUserDto.password()));
+            usuario.setRoles(Set.of(roleBasic));
 
             this.usuarioService.criar(usuario);
 
